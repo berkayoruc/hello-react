0520751c0ae708f413330f2d5fbe8c66e48605bb
berkay <berkayoruc@hotmail.com>
2021-12-23 00:08:03 +0300

    jsx typing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// import react from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  // return react.createElement("div", null, "hello");
   return (
     <div>
       <Header />
