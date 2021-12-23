194d4c0339e284961ee56ae24e9db6f64faa559a
berkay <berkayoruc@hotmail.com>
2021-12-23 13:25:38 +0300

    isrequired

--- src/components/User.js
+++ src/components/User.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function User({ name, surname, isLoggedIn, age, friends }) {
   return (
     <>
@@ -9,5 +11,13 @@ function User({ name, surname, isLoggedIn, age, friends }) {
     </>
   );
 }
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string,
+  isLoggedIn: PropTypes.bool,
+  age: PropTypes.number,
+  friends: PropTypes.array,
+};
 
 export default User;
