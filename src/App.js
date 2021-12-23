bb6eb2b58bdfb3d49175869fbef0b988a0e6b6e2
berkay <berkayoruc@hotmail.com>
2021-12-23 13:16:17 +0300

    key prop

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@
 import "./App.css";
 import User from "./components/User.js";
 
+const friends = [
+  { name: "Ahmet", id: 1 },
+  { name: "Mehmet", id: 2 },
+  { name: "Selami", id: 3 },
+];
+
 function App() {
   // return react.createElement("div", null, "hello");
   return (
     <>
-      <User name="Berkay" surname="Oruç" isLoggedIn={true} age={27} />
+      <User
+        name="Berkay"
+        surname="Oruç"
+        isLoggedIn={true}
+        age={27}
+        friends={friends}
+      />
     </>
   );
 }
